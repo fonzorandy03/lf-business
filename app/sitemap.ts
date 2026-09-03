a2d3439e8528fe7d081478d127002d8a1358e0c3
Alfonso Maria Ferrara <alfonso.mferrara@gmail.com>
2026-09-03 21:25:15 +0200

    Prepare static export for Server.it hosting

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,6 +2,8 @@ import type { MetadataRoute } from 'next'
 import { getAllArticles } from '@/lib/blog'
 import { absoluteUrl } from '@/lib/site'
 
+export const dynamic = 'force-static'
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const staticPages = ['', '/sezione-business', '/galleria', '/blog', '/contattaci'].map(
     (path) => ({
